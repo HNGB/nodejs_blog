d4abb88374fa291b83102ae9571e6729036b881f
HNGB <huynhngogiabao2211@gmail.com>
2024-04-22 09:57:46 +0700

    refactor code: routes, app/controller folder

---
+++ src/app/controllers/NewsController.js
@@ -0,0 +1,11 @@
+
+class NewsController {
+
+    index(req, res) {
+
+        res.render('news');
+
+    }
+}
+
+module.exports = new NewsController
--- src/index.js
+++ src/index.js
@@ -5,7 +5,14 @@ const engine = require('express-handlebars').engine
 const app = express()
 const port = 3000
 
+const route = require('./routes')
+
 app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(express.urlencoded({
+  extended: true
+}))
+app.use(express.json())
 
 app.use(morgan('combined'))
 
@@ -14,13 +21,7 @@ app.engine('hbs', engine({extname: '.hbs'}));
 app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, 'resources/views'))
 
-app.get('/', (req, res) => {
-  res.render('home')
-})
-
-app.get('/news', (req, res) => {
-  res.render('news')
-})
+route(app)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
